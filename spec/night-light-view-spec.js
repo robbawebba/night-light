7cab070d03eb2fcf7c15361466e2f471dcf9ea2c
Rob Weber <robertweber95@gmail.com>
2017-03-04 00:32:59 -0500

    Init new atom package

---
+++ spec/night-light-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NightLightView from '../lib/night-light-view';
+
+describe('NightLightView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
